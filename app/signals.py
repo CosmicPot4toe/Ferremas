62c5a425d49d09f06f29f620d1f169bf775e5e17
CosmicPot4toe <ji.figueroa.lopez@gmail.com>
2024-05-18 16:28:47 -0400

    nothing is going millhouse
    honestly the api is usefull, the only thing is that it doesnt pass a copy of the foreing key obj and cant do filters
    
    usefull for a backup but thats all :shrugs:

--- app/signals.py
+++ app/signals.py
@@ -2,7 +2,7 @@
 from django.dispatch import receiver
 from django.db.models.signals import pre_save, post_delete
 from .models import Producto
-from .apis import *
+from .utils.apis import *
 
 @receiver(pre_save,sender=Producto)
 def handle_Product(sender,instance:Producto, **kargs):
@@ -11,13 +11,9 @@ def handle_Product(sender,instance:Producto, **kargs):
 		#get the attribute that changed and save the new value on a dict
 		update={"id":instance.pk}
 		for k in instance.tracker.changed:
-			#I couldn't change the name of the fk in the model so I change it in the dict thats getting sent
-			if k =="categoria_id":
-				update["id_C"]=getattr(instance,k)
-				continue
 			update[k]=getattr(instance,k)
 		#havent connected to the api cuz i dont ahve it on, gotta get a host lol
-		print(update)#Phpapi('Producto').put(update)
+		PhpApi('Producto').put(update)
 
 @receiver(post_delete,sender=Producto)
 def handleProductDel(sender, **kargs):
