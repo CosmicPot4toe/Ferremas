cfc462d5ff95b65e12488a8791fdffac13694300
CosmicPot4toe <ji.figueroa.lopez@gmail.com>
2024-05-20 20:09:50 -0400

    yep
    yeppers

--- app/apps.py
+++ app/apps.py
@@ -1,9 +1,15 @@
 from django.apps import AppConfig
-
+from django.db.models.signals import pre_save, pre_delete
 
 class AppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'app'
-    def ready(self):
-        # Implicitly connect signal handlers decorated with @receiver.
-        from . import signals
+		default_auto_field = 'django.db.models.BigAutoField'
+		name = 'app'
+		def ready(self):
+				# Implicitly connect signal handlers decorated with @receiver.
+				from .signals import handle_pre_save,handle_Del
+				from .models import Categoria, CategoriaProducto, Producto, Stock, Tienda
+				
+				modelList=[Producto,CategoriaProducto,Categoria,Tienda,Stock]
+				for model in modelList:
+					pre_save.connect(handle_pre_save,sender=model)
+					pre_delete.connect(handle_Del,sender=model)
--- app/signals.py
+++ app/signals.py
@@ -1,10 +1,9 @@
 #signalis xd
 from django.dispatch import receiver
-from django.db.models.signals import pre_save, pre_delete
 from .models import Categoria, CategoriaProducto, Producto, Stock, Tienda
 from .utils.apis import *
+import json
 
-modelList=[Producto,CategoriaProducto,Categoria,Tienda,Stock]
 modelTypes=Producto|CategoriaProducto|Categoria|Tienda|Stock
 
 def handle_pre_save(sender,instance:modelTypes, **kargs):
@@ -18,26 +17,24 @@ def handle_pre_save(sender,instance:modelTypes, **kargs):
 		#havent connected to the api cuz i dont ahve it on, gotta get a host lol
 		print(PhpApi(MODEL_NAME).put(update))
 	else:
-		insert={}
+		vals={}
 		all_fields = [f.name for f in instance._meta.fields]
 		for n in all_fields:
 			match n:
-				case 'id_producto'|'id'|'id_tienda'|'id_categoria':
+				case "id_producto"|"id"|"id_tienda"|"id_categoria":
 					#todavia no guardamos en la bdd asiq la id no existe
 					continue
-				case 'categoria'|'categoria_id'|'producto'|'sucursal':
+				case "categoria"|"categoria_id"|"producto"|"sucursal":
 					#tengo un foreing key dame su id
-					insert[n]=getattr(instance,n).pk
+					vals[n]=getattr(instance,n).pk
 					continue
-			insert[n]=getattr(instance,n)
-		print(PhpApi(MODEL_NAME).post(insert))
+			vals[n]=getattr(instance,n)
+		PhpApi(MODEL_NAME).post(vals)
 
 def handle_Del(sender,instance:modelTypes, **kargs):
 	MODEL_NAME=instance._meta.model.__name__
-	print(PhpApi(MODEL_NAME).Del(instance.pk))
+	PhpApi(MODEL_NAME).Del(instance.pk)
 	#print(MODEL_NAME,instance.pk)
 
 
-for model in modelList:
-	pre_save.connect(handle_pre_save,sender=model)
-	pre_delete.connect(handle_Del,sender=model)
+
